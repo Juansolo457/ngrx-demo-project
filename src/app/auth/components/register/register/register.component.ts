e0dc81cb075d3fbb508389121429db9e5c31a8e8
Juan Juarez <isitluk@gmail.com>
2022-03-13 19:33:19 -0600

    added form group / builder, onSubmit and init form methods. Todo for later

--- src/app/auth/components/register/register/register.component.ts
+++ src/app/auth/components/register/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup; // TODO: lookup with using 'new' to instantiate formGroup
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeForm()
   }
 
+  initializeForm(): void {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(): void {
+    console.log('submit', this.form.value, this.form.valid);
+  }
 }
